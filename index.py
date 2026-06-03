2a2ad59ef1abf32b98c2f3877ab74b8dbc2b1d50
V-5000-dev <victor101001000@gmail.com>
2026-06-02 22:33:33 -0400

    commit

--- index.py
+++ index.py
@@ -5,26 +5,23 @@ import os
 
 load_dotenv('file.env')
 
-bot = discord.Bot()
 bot = commands.Bot(command_prefix='d!')
 
-#Commands
-#Ping
-@bot.event
-async def ping(ctx):
+#commands
+#ping
+async def ping_logic():
     return 'Pong!'
-
 @bot.command()
 async def ping(ctx):
-    await ctx.send(await ping(ctx))
-
-@bot.slash_command(name="ping")
+    await ctx.send(await ping_logic())
+@bot.slash_command(name="ping", description="Replies with Pong!")
 async def ping_slash(ctx):
-    await ctx.respond(await ping(ctx))
+    await ctx.respond(await ping_logic())
 
 
-
-
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
 bot.run(os.getenv('TOKEN'))
 
 

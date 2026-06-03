bb30374638fb91429e3545b3dc500418dd075222
V-5000-dev <victor101001000@gmail.com>
2026-06-02 22:43:32 -0400

    changes

--- index.py
+++ index.py
@@ -9,14 +9,14 @@ bot = commands.Bot(command_prefix='d!')
 
 #commands
 #ping
-async def ping():
+async def ping_logic():
     return 'Pong!'
 @bot.command()
-async def ping_prefix(ctx):
-    await ctx.send(await ping())
-@bot.slash_command(name="ping", description="Replies with Pong!")
+async def ping(ctx):
+    await ctx.send(await ping_logic())
+@bot.slash_command(name="ping", description="Check if the application is online.")
 async def ping_slash(ctx):
-    await ctx.respond(await ping())
+    await ctx.respond(await ping_logic())
 
 
 @bot.event

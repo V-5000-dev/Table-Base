36e3de221b6573967c2f5ee5d753984df6a05d46
V-5000-dev <victor101001000@gmail.com>
2026-06-04 12:12:51 -0400

    commit

--- index.py
+++ index.py
@@ -1,7 +1,19 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+from dotenv import load_dotenv
 import os
+import logging
+from enum import Enum
+
+#logging setup------------------------------------------------
+logging.basicConfig(
+    filename="bot.log",
+    level=logging.INFO,
+    format="%(asctime)s %(message)s"
+)
+#setup------------------------------------------------
+load_dotenv()
 
 ERROR = "<:Error:1511925664910147607>"
 CHECK = "<:CheckMark:1512108503857238076>"
@@ -12,21 +24,68 @@ DANDER = "<:DangerCommand:1511923466016657438>"
 
 GUILD_ID = discord.Object(id=1324223207536070697)
 
-intents = discord.Intents.default()
-intents.message_content = True  # Fixed typo: messages_content -> message_content
 
-bot = commands.Bot(command_prefix="db", intents=intents)
+class CommandType(Enum):
+    NORMAL = "Normal"
+    MODIFICATION = "Modification"
+    DANGER = "Danger"
+
+
+NORMAL_LOG_CHANNEL = ""
+MODIFICATION_LOG_CHANNEL = ""
+DANGER_LOG_CHANNEL = ""
+
+
+SEVER_ADMIN_ROLES = []
+#Commands ------------------------------------------------
+
+def verifyCommandPermissons(command_type: CommandType, *required_roles: list):
+    async def predicate(interaction: discord.Interaction) -> bool:
+
+        if not required_roles:
+            return True
+        if interaction.user.guild_permissions.administrator:
+            return True
+        
+        if any(role_id in required_roles for role_id in user_role_ids):
+            await interaction.response.send_message(f"{PERMISSON} You do not have permissons to use this Command.")
+            
+            logging.info(f"{command_type.value}{interaction.command.name}{interaction.user}")
+            return False
+        return app_commands.check(predicate)
+
+
+    
+        
+        
+                    
+
 class Client(commands.Bot):
     async def setup_hook(self):
-        await self.tree.sync(guild=GUILD_ID)
-        print("Commands synced.")
+        try:
+            self.tree.clear_commands(guild=GUILD_ID)
+            synced = await self.tree.sync(guild=GUILD_ID)
+            print(f"Synced {len(synced)} commands.")
+        except Exception as e:
+            print(f"Error syncing commands: {e}")
 
     async def on_ready(self):
-        print(f'{CHECK} Logged in as {self.user}.')
+        print(f'Logged in as {self.user}.')
 
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = Client(command_prefix="db", intents=intents)
 
 @bot.tree.command(name="ping", description="Checks if the application is online.", guild=GUILD_ID)
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message(f"{CHECK} Online. Pong!")
 
-bot.run(os.getenv('TOKEN'))
+
+@bot.tree.command(name="database-create", description="Create a new Database.", guild=GUILD_ID)
+async def databaseCreate(interaction: discord.Interaction, name: str):
+    if()
+    await interaction.response.send_message(f"{CHECK} Online. Pong!")
+
+
+bot.run('MTUxMTM5NjM0MzY4MjMwMjEyMg.GwWrS0.ZLAct5APdxXIkzlhIHBJK98MiEzXYe9jXbIEhg')

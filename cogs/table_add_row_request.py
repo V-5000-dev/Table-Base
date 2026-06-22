5b323daa56e69cf36b72771bce2d84999328d0aa
V-5000-dev <victor101001000@gmail.com>
2026-06-22 09:47:04 -0400

    commit

--- cogs/table_add_row_request.py
+++ cogs/table_add_row_request.py
@@ -20,7 +20,7 @@ class AddRequest(discord.ui.View):
             title=f"{action} Row Request - {self.table['name']}",
             color=color
         )
-        embed.description(text=f"Status: {status}")
+        embed.add_field(text=f"Status: {status}")
 
         old_row = self.table["data"][self.existing_index] if self.existing_index is not None else [None] * len(self.new_row)
 
